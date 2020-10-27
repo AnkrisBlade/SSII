4f993a2458c11ebf36ac094ba895173a5cdbb1d8
Francisco Sánchez <new.francisco.1998@tuta.io>
2020-10-27 17:59:25 +0100

    estadisticas 1

--- HIDS/main.py
+++ HIDS/main.py
@@ -93,15 +93,20 @@ def main():
     while True:
         print("Comprobando Integridad")
         
+        ficheros_corruptos = 0
+        ficheros_no_encontrados = 0
+        ficheros_total = len(hashes)
+        
         for ruta,hash in hashes:
+            
             try:
                 file_hash = hashlib.sha1(open(ruta).read().encode()).hexdigest()
             except FileNotFoundError as e:
-                msg = "===# FICHERO BORRADO! #===\n" \
+                msg = "===# FICHERO NO ENCONTRADO! #===\n" \
                         "Ruta: " + ruta
                 print(msg)
                 logging.error("Fallo en:(" + ruta + ") El fichero no se encuntra")
-                
+                ficheros_no_encontrados = ficheros_no_encontrados + 1
                 continue
 	
             new_hash = hashlib.sha1((file_hash + contra_raw).encode()).hexdigest()
@@ -113,9 +118,19 @@ def main():
                 print(msg)
                 #_ = messagebox.showerror("ARCHIVO CORRUPTO!", msg)
                 logging.error("Fallo en:(" + ruta + ") Hash original:(" + hash + ") Actual:(" + new_hash + ")")
+                ficheros_corruptos = ficheros_corruptos + 1
             
-            logging.info("Integridad comprobada")
+        logging.info("Integridad comprobada")
+        
+        porcentaje_corruptos = float(ficheros_corruptos/ficheros_total)*100
+        porcentaje_no_encontrados = float(ficheros_no_encontrados/ficheros_total)*100
              
+        logging.info("Estadisticas: corruptos: " + str(porcentaje_corruptos) \
+                    + "% no encontrados: " + str(porcentaje_no_encontrados) + "%")     
+        
+        print("FICHEROS: " + str(ficheros_total))
+        print("CORRUPTOS: " + str(ficheros_corruptos) + " " + str(porcentaje_corruptos)+"%")
+        print("NO ENCONTRADOS: " + str(ficheros_no_encontrados) + " " + str(porcentaje_no_encontrados)+"%")
         print(" -- ")
         
         time.sleep(intervalo)
