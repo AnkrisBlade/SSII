3231df7007d04cd03cd1490d22ca6b87212a9a89
Ankris <ventura.carriazo@gmail.com>
2020-10-28 17:11:37 +0100

    generador de hids.csv

--- HIDS/hids_gen.py
+++ HIDS/hids_gen.py
@@ -6,21 +6,24 @@ import csv
 
 # Returns list with all files inside that path
 def filepaths(archivo_path):
-    path = csv.reader(archivo_path, delimiter=',')
-    paths = []
-    for i in path:
-        for subdir, _, files in os.walk(i):
-            for file in files:
-                paths.append(os.path.join(subdir, file))
+    with open(archivo_path) as csv_file:
+        path = csv.reader(csv_file)
+        paths = []
+        for i in path:
+            for subdir, _, files in os.walk(i[0]):
+                for file in files:
+                    paths.append(os.path.join(subdir, file))
         return paths
 
-paths = filepaths(r"D:\Fork\SSII\Password_Generator")
+paths = filepaths(r"D:\Fork\SSII\HIDS\Password_Generator.csv")
 
-dir = os.path.dirname(paths[0]).split("\\")[-1]
+def genfile(name, paths, password):
+    with open(name, 'w', newline='') as csv_file:
+        hasheo = csv.writer(csv_file, delimiter=',', quotechar='"', quoting=csv.QUOTE_MINIMAL)
+        for i in paths:
+            file_hash = hashlib.sha1(open(i).read().encode()).hexdigest()
+            hasheo.writerow([i, " " + hashlib.sha1((file_hash + password).encode()).hexdigest()])
 
-with open(dir+".csv", 'w', newline='') as myfile:
-    wr = csv.writer(myfile, quoting=csv.QUOTE_ALL)
-    for i in paths:
-        wr.writerow(i + ", " + hashlib.sha512(i.encode('utf-8')).hexdigest())
+    return "Hasheo de archivos finalizado"
 
-
+genfile("hids_test.csv", paths, "test")
